e268ab62e46b515ad1075c0ff6a330c58b9af2f9
Fariba DK <faribacodes@gmail.com>
2021-07-07 09:14:36 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express()
+const app = express();
 
 app.get('/', (req, res)=>{
   res.send({ hi: 'there' })
-})
+});
 
 const PORT = process.env.PORT || 4960;
 //In PRODUCTION, See if heroku providing a PORT || But if we are running on our sever then use 5000 in DEVELOPMENT env
